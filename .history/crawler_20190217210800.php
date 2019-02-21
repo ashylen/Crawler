90cbb6e7d47902f91c8ffee24c75410c9b8aefb2
Ashylen <ashylen@o2.pl>
2019-02-21 18:32:27 +0100

    Fixed loop in createCSV() method and added detection for invalid lawyers to import.

--- .history/crawler_20190217210800.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$website = 'http://www.rejestradwokatow.pl/adwokat/ewidencja/szukaj/Szukaj';
--- crawler.php
+++ crawler.php
@@ -1,7 +1,7 @@
 <?php
 
 //Rest API link to page
-$website = 'http://www.rejestradwokatow.pl/adwokat/szczegoly/id/49965';
+$website = 'http://www.rejestradwokatow.pl/adwokat/szczegoly/id/';
 
 //Hide warnings
 libxml_use_internal_errors(true);
@@ -37,10 +37,17 @@ function getPageContent($url)
     foreach($tableData as $key => $td)
     {
         //Trim whitespaces
-        $header = trim($tableHeaders[$key]->nodeValue);
+        if(!empty($tableHeaders[$key]))
+            $header = trim($tableHeaders[$key]->nodeValue);
+
+
+
+
         //Remove excessing chars
         $header = str_replace(':', '', $header);
-        $nodes[str_replace(':','', $header)] =  trim($td->nodeValue);
+        dump(trim($td->nodeValue));
+        if ($header == 'Status:' && trim($td->nodeValue) == "Wykonujący zawód")
+            $nodes[str_replace(':','', $header)] =  trim($td->nodeValue);
     }
 
     return $nodes;
@@ -50,30 +57,38 @@ function getPageContent($url)
 //Create CSV from page content
 function createCSV($data)
 {
-    dump($data); die;
+
     header('Content-Type: text/csv');
     header('Content-Disposition: attachment; filename="sample.csv"');
 
 
     $fp = fopen('php://output', 'wb');
+    fprintf($fp, chr(0xEF) . chr(0xBB) . chr(0xBF));
     $i = 0;
-    foreach ($data as $key => $line) {
-        if($i == 0)
-        {
+    $headers = [];
+    $fields = [];
+    foreach ($data as $key => $field) {
 
-        }
+        $headers[] = $key;
+        $fields[] = $field;
 
-        fputcsv($fp, $line);
         $i++;
     }
+
+    fputcsv($fp, $headers);
+    fputcsv($fp, $fields);
     fclose($fp);
 }
 
-// for($i = 40000; $i < 45000; $i++)
-// {
-//     echo ' xd'. $i;
-// }
+$data = [];
+for($i = 49960; $i < 49966; $i++)
+{
+    $data[] = getPageContent($website.$i);
+
+}
+
+dump($data); die;
 
 
-$data = getPageContent($website);
+
 createCSV($data);
